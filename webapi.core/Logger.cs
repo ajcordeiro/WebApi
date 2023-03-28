625434dff7ef76160f5bd48208ed3b8ee9d52de0
Jaqueson Cordeiro Alves <p22.jaqueson@webmotors.com.br>
2023-03-28 16:34:00 -0300

    Add project files.

---
+++ webapi.core/Logger.cs
@@ -0,0 +1,27 @@
+﻿using webapi.core.contracts;
+
+namespace webapi.core
+{
+    public class Logger : ILoggerManager
+    {
+        public void LogDebug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
