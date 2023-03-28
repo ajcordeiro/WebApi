4f0118e13fee391e64bd67f25b1ee441332657de
Jaqueson Cordeiro Alves <p22.jaqueson@webmotors.com.br>
2023-03-28 18:22:04 -0300

    Add project files

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 
@@ -8,7 +14,20 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
